855f2108a45e57ab71f0a284f9150c8566ac9d92
GlebSolovev <lionyoing@mail.ru>
2019-11-15 12:52:03 +0300

    Hi there

--- practice02/facts.py
+++ practice02/facts.py
@@ -7,6 +7,8 @@ Person = collections.namedtuple('Person', ['github_login', 'first_name', 'last_n
 people = [
     Person(github_login='example', first_name='Name', last_name='Surname',
            facts=['Fact-right.', 'Fact-write.', 'Fact-right.']),
+    Person(github_login='GlebSolovev', first_name='Gleb', last_name='Solovev',
+           facts=['Hate programming.', 'Hate Python.', 'Hate Linux.'])
 ]
 
 answers = {}
